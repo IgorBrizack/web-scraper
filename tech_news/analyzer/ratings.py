b6890e2ca5cc8e1734ff71dc4d17145276f4a9fb
Igor Brizack <igor_brizack@hotmail.com>
2023-02-09 17:31:25 -0300

    requisito 10 concluido

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,37 @@
+from tech_news.database import search_news
+
+
+def sort_items(items):
+    rank = sorted(items)
+    rank = sorted(rank, key=lambda rank: -rank[1])
+
+    result = []
+    for items in rank:
+        result.append(items[0])
+    return result
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = search_news({})
+    categories = []
+    helper = 0
+    counter = 0
+
+    for content in news:
+        categories.append(content['category'])
+
+    only_categories = set(categories)
+
+    rank = []
+
+    for category in only_categories:
+        while helper < len(categories):
+            if category == categories[helper]:
+                counter += 1
+            helper += 1
+        rank.append((category, counter))
+        counter = 0
+        helper = 0
+
+    return sort_items(rank)
