6bbd2b44c02e08e1ec3676c51bb4f400cdc09d77
Igor Brizack <igor_brizack@hotmail.com>
2023-02-09 13:02:39 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime as d
 
 
 # Requisito 7
@@ -15,6 +16,21 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
+    try:
+        converted_data = (d.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y"))
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    news = search_news({'timestamp': converted_data})
+    result = []
+
+    if len(news) == 0:
+        return []
+
+    for content in news:
+        result.append((content['title'], content['url']))
+
+    return result
     """Seu código deve vir aqui"""
 
 
