f5e112ae411ebadfa1bf4be263d9ec1b8e65254d
Igor Brizack <igor_brizack@hotmail.com>
2023-02-09 12:38:31 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({})
+    result = []
+
+    for content in news:
+        if title.lower() in content['title'].lower():
+            result.append((content['title'], content['url']))
+
+    return result
 
 
 # Requisito 8
